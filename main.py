1fbb6b611e7095df44e9b42e9019c299e3b9c8f5
Daniel Rodriguez <mdaniel.r543@gmail.com>
2023-08-10 10:40:50 -0600

    se inicia el ejemplo

---
+++ main.py
@@ -0,0 +1,17 @@
+import xml.etree.ElementTree as ET
+
+# Cargando el XML desde el archivo
+tree = ET.parse('datos.xml')
+root = tree.getroot()
+
+# Recorriendo el árbol XML
+for elemento in root.findall('elemento'):
+    nombre = elemento.get('nombre')
+    id_val = elemento.get('id')
+    print(f'Elemento: Nombre={nombre}, ID={id_val}')
+
+    for item in elemento.findall('item'):
+        value = item.get('value')
+        text = item.text
+        print(f'    Item: Value={value}, Text={text}')
+
